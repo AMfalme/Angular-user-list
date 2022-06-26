936f231534fee7d8ef7d1475177a956d9fb658cf
Griffin <mfalmegriffin@gmail.com>
2022-06-26 17:51:08 +0300

    create global sass folder generate new users-list component

---
+++ src/app/users-list/users-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-users-list',
+  templateUrl: './users-list.component.html',
+  styleUrls: ['./users-list.component.scss']
+})
+export class UsersListComponent implements OnInit {
+  @Input() users: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
